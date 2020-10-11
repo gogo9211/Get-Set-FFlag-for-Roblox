fe4a0cf91f6e34056e61626680156804ab5dfde2
gogo9211 <49488824+gogo9211@users.noreply.github.com>
2020-10-11 21:49:19 +0300

    Add files via upload

---
+++ Get Set FFlag.h
@@ -0,0 +1,38 @@
+//To find them in IDA u can search for "_DataCenterFilter" in strings. Its used in Setfflag and the actual name is FLog::SetValue
+//GetValue is close in memory to SetValue so you should be able to find it
+
+
+typedef bool(__cdecl* FLog)(const std::string& name, std::string& value, bool alsoCheckUnknown);
+FLog GetValue = (FLog)x(0x15A5860);
+
+typedef bool(__cdecl* SLog)(const std::string& name, const std::string& value, int Type, bool loadedFromServer);
+SLog SetValue = (SLog)x(0x15A6030);
+
+int GetFFlag(int ls)
+{
+	std::string flag = Roblox::r_lua_tostring(ls, 1);
+	std::string ret;
+
+	bool out = GetValue(flag, ret, 1);
+	if (!out)
+		Roblox::r_luaL_error("Flag not found!");
+
+	Roblox::r_lua_pushstring(ls, ret.c_str());
+	return 1;
+}
+
+int SetFFlag(int ls)
+{
+	std::string flag = Roblox::r_lua_tostring(ls, 1);
+	std::string value = Roblox::r_lua_tostring(ls, 2);
+	std::string ret;
+
+	bool exist = GetValue(flag, ret, 1);
+
+	if (!exist)
+		Roblox::r_luaL_error("Flag not found!");
+
+	for (int i = 0; i <= 4; i++)
+		SetValue(flag, value, i, 0);
+	return 0;
+}
